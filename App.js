83055fabfb7a0ee974b2b585e13be3dc1a96b96a
Shubham Singh <shubham@geekyants.com>
2017-10-31 18:28:06 +0530

    orientation bug fixed

--- App.js
+++ App.js
@@ -3,8 +3,11 @@ import { Provider } from "react-redux";
 import { Root } from "native-base";
 import NavigationDetails from "./js/Navigator";
 import store from "./js/store";
-console.ignoredYellowBox = ["Setting a timer", "Warning: Can only update"];
+import Orientation from "react-native-orientation";
 class App extends React.Component {
+  componentWillMount() {
+    Orientation.lockToPortrait();
+  }
   render() {
     return (
       <Root>
